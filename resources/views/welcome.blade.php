a93b8893b689d4d64655347203aa76352716e8f7
PrdpPkrl25 <pradip.pokhrel25@gmail.com>
2020-05-22 10:05:57 +0545

    Landing Page Ready

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,16 +7,17 @@
 
     <link href="/css/app.css" rel="stylesheet">
     <link href="/welcome/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/fontawesome.min.css" rel="stylesheet">
+
 
 
 </head>
 
 <body>
-<nav class="navbar  navbar-fixed-top navbar-expand-md " role="navigation">
+<nav class="navbar  navbar-fixed-top navbar-light navbar-expand-lg " role="navigation">
     <div class="container-fluid">
         <a class="navbar-brand" href="{{ url('/') }}"> <img height="80px" width="auto" src="{{asset('img/Wisherr.png')}}" alt="Website Logo"> </a>
             <button type="button" class="navbar-toggler " data-toggle="collapse" data-target="#navbar-collapse-main">
@@ -46,7 +47,7 @@
 
 <div id="home">
     <div class="landing-text">
-        <h1>WISHERR</h1>
+        <h1>WELCOME TO WISHERR</h1>
         <h3>Setup Message For Important Upcoming Event</h3>
         <a href="{{ route('login') }}" class="btn btn-default btn-lg">Start Creating</a>
     </div>
@@ -55,9 +56,14 @@
 <div class="padding">
     <div class="container">
         <div class="row text-center">
-            <h1>Want to know about us?</h1>
-            <p class="lead"> You can now create event of your keen person and preset the message so you will never forget as we will send the pre set message on your behalf.</p>
-            <p class="lead"> You can send the message at any number of time on the day of the event. Now You wont have to be the person who keep forgetting to wish or the one who wish late.</p>
+            <h2>What is Wisherr?</h2>
+            <p class="lead">Are you a person who have trouble remembering the date?
+                            Isn't it exciting that you have to worry one less thing
+                            in your life. With wisherr, you can create upcoming event
+                            like birthday, anniversary, holiday etc. and set the message
+                            to send to any person on that day, we will send the message
+                            on your behalf.</p>
+
         </div>
     </div>
 </div>
@@ -65,36 +71,36 @@
 <div class="padding">
     <div class="container">
         <div class="row text-center">
-            <h1>Services:</h1>
+            <h2>Our Services:</h2>
            <ul class="benifit">
-               <li> Create wish for many events like anniversay,birthday,new year etc. </li>
-               <li> Send message for any no of time in a day. You can select the time. </li>
-               <li> Edit your message from pre selected template </li>
-               <li> Create your own message template or gift card and sell it in our store </li>
+               <li> * Create wish for many events like anniversay, birthday, new year etc. </li>
+               <li> * Send message for any no of time in a day. You can select the time. </li>
+               <li>  * Edit your message by choosing any template from our template store. </li>
+               <li>  * Create your own message template or gift card and sell it in our store. </li>
            </ul>
         </div>
     </div>
-</div>
-<div id="fixed">
-
 </div>
 
 <footer class="container-fluid text-center">
     <div class="row">
         <div class="col-sm-6">
-            <h1>Contact Us:</h1>
+            <h2>Contact Us:</h2>
             <p> Pradip Pokhrel<br>
                 +9779864332324<br>
                 wisherr.service@gmail.com<br>
                 Kohakpur-11,Banke Nepal</p>
         </div>
         <div class="col-sm-6">
-         <h3>Connect Us</h3>
+         <h2>Connect Us</h2>
             <a href="#" class="fa fa-facebook"></a>
             <a href="#" class="fa fa-instagram"></a>
             <a href="#" class="fa fa-linkedin"></a>
             <a href="#" class="fa fa-youtube"></a>
         </div>
+    </div>
+    <div class="row">
+        <p>Copyright © Wisherr 2020</p>
     </div>
 </footer>
 
