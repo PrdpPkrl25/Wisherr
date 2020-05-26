c87d5f4a5ea11c55968f3455a8cefd4fcc3e3e4f
PrdpPkrl25 <pradip.pokhrel25@gmail.com>
2020-05-27 02:10:03 +0545

    Added Create Event Button

--- resources/views/event/index.blade.php
+++ resources/views/event/index.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
         <div class="container">
+            <a id="createeventbtn" class="btn btn-primary offset-md-6" href="{{route('events.create')}}">Create New</a>
             <div class="table-wrapper">
             {!! $dataTable->table(['class' => 'table table-bordered table-hover mb-0 dt-responsive no-footer dtr-inline
                 collapsed'], true) !!}
--- resources/views/widgets/upcoming_events.blade.php
+++ resources/views/widgets/upcoming_events.blade.php
@@ -1,7 +1,7 @@
 @foreach($events as $event)
 
     <div class="col-md-6">
-        <div class="card bg-info" onclick="location.href='{{route('events.show',$event->id)}}';" style="cursor: pointer" >
+        <div class="card bg-info" onclick="location.href='{{route('events.show',$event->id)}}';" style="cursor: pointer;margin-top: 10px" >
             <div class="card-body">
                 <h2 class="card-title">{{$event->event_name}}</h2>
              </div>
